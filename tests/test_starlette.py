fcb04769f6e7486c0926bf1bca549da449238e72
Colin-b <Colin-b@users.noreply.github.com>
2020-02-19 12:45:11 +0100

    Add test cases on endpoint

---
+++ tests/test_starlette.py
@@ -0,0 +1,91 @@
+from apispec import BasePlugin
+from starlette.applications import Starlette
+from starlette.testclient import TestClient
+
+from apispec_starlette import add_swagger_json_endpoint
+
+
+def test_default_swagger_json_endpoint():
+    app = Starlette()
+    add_swagger_json_endpoint(app)
+
+    @app.route("/test")
+    def test_endpoint(request):
+        """
+        responses:
+            200:
+                description: "ok"
+        """
+        pass  # pragma: no cover
+
+    @app.route("/test_without_doc")
+    def test_endpoint_without_doc(request):
+        pass  # pragma: no cover
+
+    client = TestClient(app)
+    response = client.get("/swagger.json")
+    assert response.json() == {
+        "info": {"title": "My API", "version": "0.0.1"},
+        "paths": {
+            "/test": {
+                "get": {
+                    "operationId": "get_test_endpoint",
+                    "responses": {"200": {"description": "ok"}},
+                }
+            },
+            "/test_without_doc": {
+                "get": {"operationId": "get_test_endpoint_without_doc"}
+            },
+        },
+        "swagger": "2.0",
+    }
+
+
+def test_swagger_json_endpoint_parameters():
+    app = Starlette()
+
+    class DummyPlugin(BasePlugin):
+        pass
+
+    plugin = DummyPlugin()
+
+    spec = add_swagger_json_endpoint(
+        app,
+        title="Test API",
+        version="1.0.0",
+        plugins=[plugin],
+        info={"x-test": "test field"},
+    )
+
+    assert plugin in spec.plugins
+
+    @app.route("/test")
+    def test_endpoint(request):
+        """
+        responses:
+            200:
+                description: "ok"
+        """
+        pass  # pragma: no cover
+
+    @app.route("/test_without_doc")
+    def test_endpoint_without_doc(request):
+        pass  # pragma: no cover
+
+    client = TestClient(app)
+    response = client.get("/swagger.json")
+    assert response.json() == {
+        "info": {"title": "Test API", "version": "1.0.0", "x-test": "test field"},
+        "paths": {
+            "/test": {
+                "get": {
+                    "operationId": "get_test_endpoint",
+                    "responses": {"200": {"description": "ok"}},
+                }
+            },
+            "/test_without_doc": {
+                "get": {"operationId": "get_test_endpoint_without_doc"}
+            },
+        },
+        "swagger": "2.0",
+    }
